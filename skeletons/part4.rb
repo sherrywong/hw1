b877c07564040bd565ab88617d44e05ce1a36a5b
sherrywong <sherrywong@berkeley.edu>
2012-09-03 12:43:49 -0700

    Update skeletons/part4.rb

--- skeletons/part4.rb
+++ skeletons/part4.rb
@@ -1,23 +1,39 @@
 class Dessert
   def initialize(name, calories)
     # YOUR CODE HERE
+    @name = name
+    @calories = calories
   end
   
   def healthy?
     # YOUR CODE HERE
+    if @calories < 200
+      return true
+    else 
+      return false
+    end
   end
   
   def delicious?
     # YOUR CODE HERE
+    true
   end
 end
 
 class JellyBean < Dessert
   def initialize(name, calories, flavor)
     # YOUR CODE HERE
+    @name = name
+    @calories = calories
+    @flavor = flavor
   end
   
   def delicious?
     # YOUR CODE HERE
+    if @flavor.casecmp("black licorice")
+      return false
+    else
+      return true
+    end
   end
 end
