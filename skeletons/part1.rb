a4336cf814d9d1e6d9ae7d248964f2a00241ad24
sherrywong <sherrywong@berkeley.edu>
2012-09-03 12:36:10 -0700

    Update skeletons/part1.rb

--- skeletons/part1.rb
+++ skeletons/part1.rb
@@ -1,10 +1,10 @@
 def palindrome?(str)
   # YOUR CODE HERE
   str_reverse = str.reverse
-  if str_reverse == str then
-    true
+  if str_reverse.casecmp(str) == 0 then
+    return true
   else
-    false
+    return false
   end
 end
 
@@ -12,7 +12,7 @@ def count_words(str)
   # YOUR CODE HERE
   hash = Hash.new(0)
   str.each do |word|
-    hash[word] += 1
+    hash[word.downcase] += 1
   end
   hash
 end
