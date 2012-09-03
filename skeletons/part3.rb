01dd7b60c89be28db1da63b569c83512df7ba8fc
sherrywong <sherrywong@berkeley.edu>
2012-09-03 12:33:19 -0700

    Update skeletons/part3.rb

--- skeletons/part3.rb
+++ skeletons/part3.rb
@@ -1,3 +1,15 @@
 def combine_anagrams(words)
  # YOUR CODE HERE
+ hash = Hash.new()
+ words.each do |word|
+  sorted_word = word.sort.downcase
+  if hash[sorted_word] != nil
+   hash[sorted_word] << word
+  else
+   hash[sorted_word] = [word]
+  end
+ end
+ result = []
+ hash.each do |key, value|
+ result << value
 end
