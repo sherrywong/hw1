0ff13871e181cff104a7300d85e17fde5a53f3cf
sherrywong <sherrywong@berkeley.edu>
2012-09-04 16:55:26 -0700

    Update skeletons/part6.rb

--- skeletons/part6.rb
+++ skeletons/part6.rb
@@ -12,8 +12,22 @@ end
 
 class String
   # YOUR CODE HERE
+  def palindrome?
+    if self.casecmp(self.reverse.downcase) == 0 then
+      return true
+    else
+      return false
+    end
+  end
 end
 
 module Enumerable
   # YOUR CODE HERE
+  def palindrome?
+    if self.to_a == reverse_each(self).to_a then
+      return true
+    else
+      return false
+    end
+  end
 end
