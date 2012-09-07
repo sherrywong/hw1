e8680c062ca70f32be6a543bbc142c60be1994c3
sherrywong <sherrywong@berkeley.edu>
2012-09-07 07:41:00 -0700

    Update skeletons/part6.rb

--- skeletons/part6.rb
+++ skeletons/part6.rb
@@ -1,11 +1,19 @@
 class Numeric
-  @@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019}
+  @@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019, 'dollar' => 1.000}
   def method_missing(method_id)
+    puts method_id.to_s
     singular_currency = method_id.to_s.gsub( /s$/, '')
     if @@currencies.has_key?(singular_currency)
       self * @@currencies[singular_currency]
+    #the 'in(:currency) method
+    elsif singular_currency.include?('in')
+      currency = method_id.to_s.gsub(/[(]/, '')
+      currency = currency.to_s.gsub(/[)]/, '')
+      singular_currency = currency.to_s.gsub(/s$/, '')
+      rate = 1 / @@currencies[singular_currency]
+      self * rate
     else
-      super
+      super    
     end
   end
 end
