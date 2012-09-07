b6c8204b1101d90ce15bdce8e8c02825f3fe1aa2
sherrywong <sherrywong@berkeley.edu>
2012-09-07 06:19:23 -0700

    Update skeletons/part5.rb

--- skeletons/part5.rb
+++ skeletons/part5.rb
@@ -3,9 +3,18 @@ class Class
     attr_name = attr_name.to_s   # make sure it's a string
     attr_reader attr_name        # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
-    hash_attr = Hash.new(0)
-    class_eval ("def #{attr_name}; @#{attr_name}; end")
-    class_eval ("def #{attr_name}=(val); @#{attr_name} = val; #{hash[@attr_name] << val}; end"
-    class_eval ("def #{attr_name+'_history'}; return #{hash[@attr_name]}; end")
-    end
+    history = attr_name+'_history'
+   
+    #Retrun [nil] when no values assigned?
+    class_eval %Q{
+                  def #{attr_name}=(val) 
+                    @#{attr_name} = val 
+                    if @#{history}.nil? then 
+                      @#{history} = [nil, val] 
+                    else
+                      @#{history} << val 
+                    end
+                  end
+                   } 
+  end
 end
