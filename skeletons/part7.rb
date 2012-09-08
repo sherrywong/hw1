0ba69ded9785ecde304d04961a512568527f1e3b
sherrywong <sherrywong@berkeley.edu>
2012-09-07 18:48:45 -0700

    Update skeletons/part7.rb

--- skeletons/part7.rb
+++ skeletons/part7.rb
@@ -1,4 +1,22 @@
 class CartesianProduct
   include Enumerable
   # YOUR CODE HERE
+  attr_accessor :collection1, :collection2
+  
+  def initialize (collection1, collection2)
+    @collection1 = collection1
+    @collection2 = collection2
+  end
+  
+  def each
+    if @collection1.empty? or @collection2.empty?
+      yield []
+    else
+      for i in 0..collection1.length-1
+        for j in 0..collection2.length-1
+          yield [collection1[i], collection2[j]]
+        end
+      end
+    end
+  end
 end
