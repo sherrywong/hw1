e0163f9f34ce0796af07cc54e09953f353d6b15d
sherrywong <sherrywong@berkeley.edu>
2012-09-08 09:36:27 -0700

    Update skeletons/part6.rb

--- skeletons/part6.rb
+++ skeletons/part6.rb
@@ -1,22 +1,22 @@
 class Numeric
   @@currencies = {'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019, 'dollar' => 1.000}
-  def method_missing(*arguments)
+  def method_missing(method_id)
     money = 0.0
-    arguments.each do |method_id|
+  #  arguments.each do |method_id|
      singular_currency = method_id.to_s.gsub( /s$/, '')
       if @@currencies.has_key?(singular_currency)
-        money = self * @@currencies[singular_currency]
+  money = self * @@currencies[singular_currency]
       else
-        super    
+        super
       end
-    end
+  #  end
     return money
   end
-  
+
   def in(currency)
     singular_currency = currency.to_s.gsub(/s$/, '')
       if @@currencies.has_key?(singular_currency)
-        self / @@currencies[singular_currency]
+	self / @@currencies[singular_currency]
       end
   end
 end
@@ -24,22 +24,15 @@ end
 class String
   # YOUR CODE HERE
   def palindrome?
-    if self.casecmp(self.reverse.downcase) == 0 then
-      return true
-    else
-      return false
-    end
-  end
-  def method_missing(method_id)
-    if method_id == 'palindrome?'
-      self.send(:palindrome?, self)
-    end
+      self.downcase.gsub(/\W/, '') == self.downcase.gsub(/\W/, '').reverse
   end
 end
 
 module Enumerable
   # YOUR CODE HERE
   def palindrome?
-      self.send(:palindrome?, self.to_s)
+     enumereable_obj = self.to_a
+     enumerable_obj == enumerable_obj.reverse
   end
 end
+
